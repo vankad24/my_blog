9cdefa99016dd018148dd92af2cf3c6ed6281712
Molo4ko <vankad24@mail.ru>
2026-08-30 21:56:44 +0800

    Fix deploy for fucking alpine linux

--- start.sh
+++ start.sh
@@ -1,33 +1,43 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
+
+
+# read -rp "Enter server name for Nginx: " SERVER_NAME
+SERVER_NAME="molo4ko-blog.h4ck.me"
+CONFIG_NAME="my_blog.conf"
+
+
+export SERVER_NAME
+
 
 if command -v apt >/dev/null 2>&1; then
     echo "Detected apt. Installing packages..."
     
     sudo apt update
     sudo apt install -y nginx certbot python3-certbot-nginx nodejs npm
-
+	sudo systemctl enable --now docker nginx
+	
+	sed "s/{SERVER_NAME}/$SERVER_NAME/g" nginx-config.template | sudo tee "/etc/nginx/sites-available/$CONFIG_NAME" > /dev/null
+	sudo ln -sfn /etc/nginx/sites-available/$CONFIG_NAME /etc/nginx/sites-enabled/$CONFIG_NAME
+	sudo systemctl reload nginx
+	
 elif command -v apk >/dev/null 2>&1; then
     echo "Detected apk. Installing packages..."
     
     sudo apk update
-    sudo apk add nginx certbot certbot-nginx nodejs npm
+    sudo apk add nginx nginx-systemd certbot certbot-nginx nodejs npm
+	sudo systemctl enable --now docker nginx
+	
+	
+	sed "s/{SERVER_NAME}/$SERVER_NAME/g" nginx-config.template | sudo tee "/etc/nginx/http.d/$CONFIG_NAME" > /dev/null
+	sudo rm -f /etc/nginx/http.d/default.conf
+	sudo systemctl reload nginx
 
 else
     echo "Error: neither apt nor apk was found."
     exit 1
 fi
-
-
-# read -rp "Enter server name for Nginx: " SERVER_NAME
-SERVER_NAME="molo4ko-blog.h4ck.me"
-PROJECT_NAME="my_blog"
-
-export SERVER_NAME
-
-sed "s/{SERVER_NAME}/$SERVER_NAME/g" nginx-config.template | sudo tee "/etc/nginx/sites-available/$PROJECT_NAME" > /dev/null
-sudo ln -sfn /etc/nginx/sites-available/$PROJECT_NAME /etc/nginx/sites-enabled/$PROJECT_NAME
 
 sudo nginx -t && sudo systemctl reload nginx
 
