d2a7d5221229726436551636c22bb9bd0360051a
Vankad24 <vankad24@mail.ru>
2026-09-06 19:09:34 +0800

    Fix converting string to color

--- frontend/src/utils/stringToHslColor.js
+++ frontend/src/utils/stringToHslColor.js
@@ -1,3 +1,14 @@
+function hashString(str) {
+  let hash = 2166136261;
+
+  for (let i = 0; i < str.length; i++) {
+    hash ^= str.charCodeAt(i);
+    hash = Math.imul(hash, 16777619);
+  }
+
+  return hash >>> 0;
+}
+
 /**
  * Генерирует пастельный HSL-цвет из строки.
  * Используется для цветных фонов тегов.
@@ -5,11 +16,7 @@
  * @returns {string} HSL-цвет в формате hsl(hue, saturation, lightness)
  */
 export function stringToHslColor(str) {
-  let hash = 0
-  for (let i = 0; i < str.length; i++) {
-    hash = (hash * 31 + str.charCodeAt(i)) | 0
-  }
-  hash = Math.abs(hash)
+  let hash = hashString(str)
   const hue = hash % 360
   return `hsl(${hue}, 80%, 90%)`
 }
