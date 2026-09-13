a4c9494f373b85d00fd9a3ea6e3b6a92df5b47eb
Vankad24 <vankad24@mail.ru>
2026-09-13 23:09:53 +0800

    Fix start script, move server name to env

--- start.sh
+++ start.sh
@@ -4,13 +4,15 @@ set -e
 
 git pull
 
-# read -rp "Enter server name for Nginx: " SERVER_NAME
-SERVER_NAME="molo4ko-blog.h4ck.me"
-CONFIG_NAME="my_blog.conf"
+# import .env
+set -a
+. ./.env
+set +a
 
-
+# make visible for child processes
 export SERVER_NAME
 
+CONFIG_NAME="$SERVER_NAME.conf"
 
 if command -v apt >/dev/null 2>&1; then
     echo "Detected apt. Installing packages..."
@@ -21,7 +23,6 @@ if command -v apt >/dev/null 2>&1; then
 	
 	sed "s/{SERVER_NAME}/$SERVER_NAME/g" nginx-config.template | sudo tee "/etc/nginx/sites-available/$CONFIG_NAME" > /dev/null
 	sudo ln -sfn /etc/nginx/sites-available/$CONFIG_NAME /etc/nginx/sites-enabled/$CONFIG_NAME
-	sudo systemctl reload nginx
 	
 elif command -v apk >/dev/null 2>&1; then
     echo "Detected apk. Installing packages..."
@@ -29,11 +30,12 @@ elif command -v apk >/dev/null 2>&1; then
     sudo apk update
     sudo apk add nginx nginx-systemd certbot certbot-nginx nodejs npm
 	sudo systemctl enable --now docker nginx
-	
+	sudo rm -f /etc/nginx/http.d/default.conf
+
+	# add updating to cron
+	sudo cp update-sertificate.sh /etc/periodic/daily/
 	
 	sed "s/{SERVER_NAME}/$SERVER_NAME/g" nginx-config.template | sudo tee "/etc/nginx/http.d/$CONFIG_NAME" > /dev/null
-	sudo rm -f /etc/nginx/http.d/default.conf
-	sudo systemctl reload nginx
 
 else
     echo "Error: neither apt nor apk was found."
@@ -50,15 +52,8 @@ docker compose up -d --build
 # Run in subshell
 (cd frontend && npm install && npm run build)
 
-echo "Do you want to update https certificates? (y/n)"
-read answer
-if [ "$answer" = "y" ]; then
-	sudo certbot --nginx -d "$SERVER_NAME"
-	sudo certbot renew --dry-run
-
-else
-	echo "Skip updating certificates"
-fi
+sudo certbot --nginx --reinstall -d "$SERVER_NAME"
+sudo certbot renew --dry-run
 
 echo backend 127.0.0.1:8000
 echo pgadmin 127.0.0.1:8080 server connection: host=postgres:5432 username=postgres
